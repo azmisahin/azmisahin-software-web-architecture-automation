e2b73423fa827dd1b9bac0290ff58ec20b4b9b1f
Azmi SAHIN <azmisahin@outlook.com>
2020-05-16 18:57:07 +0300

    api v2 unit test

--- src/controllers/api/v2/time/index.js
+++ src/controllers/api/v2/time/index.js
@@ -7,15 +7,12 @@ var express = require('express')
  */
 var api = module.exports = express.Router()
 
-// Yönlendirici adresleri
-api.use('/time', require('./time'))
-
 // Giriş isteği ayarlanıyor.
 api.get('/', function (req, res) {
 
-  // Geri dönüş değeri yapılandırılıyor.
-  // api versiyon 1
-  var result = 'v2'
+  // Geri dönüş değeri yapılandırılıyor. 
+  // Tarih ve saat ISO formatı ile hazırlanıyor.
+  var result = new Date().toISOString()
 
   // Başarılı cevabı iletiliyor ve veri gönderiliyor.
   res.status(200).send(result)
---
+++ tests/unit/api/v2/index.test.js
@@ -0,0 +1,34 @@
+/// Gerekli kütüphaneler
+var request = require('supertest')
+var api = require('@arc/controllers/api/v2')
+
+/// Mock
+var express = require('express')
+var app = express()
+
+app.use(api)
+
+/**
+ * Time api testleri
+ */
+describe('v2 api tests', function () {
+
+  // Hedeflenen sürüm.
+  var url = '/api/v2'
+
+  /**
+     * Hedeflenen sürüm için, global test.
+     */
+  it(url, function (done) {
+
+    // SuperTest talep ediliyor.
+    request(app)
+
+    // Global sürüm testi.
+      .get('/')
+
+    // Başarılı cevabı alınması gerekli.
+      .expect(200, 'v2')
+      .end(done)
+  })
+})
